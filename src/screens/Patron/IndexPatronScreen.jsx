5a83db8765d4952aef7da87b6d6550af9968691f
ngecu <devngecu@gmail.com>
2024-10-22 20:17:20 +0300

    prod env

--- src/screens/Patron/IndexPatronScreen.jsx
+++ src/screens/Patron/IndexPatronScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { GiBookshelf } from "react-icons/gi";
 import { BsEmojiAngryFill } from "react-icons/bs";
@@ -8,20 +8,46 @@ import { FaUsers } from "react-icons/fa";
 import { useFetchBooksQuery } from '../../features/booksApi';
 import { useFetchPatronsQuery } from '../../features/userApi';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 
 const IndexPatronScreen = () => {
+
 
   const location = useLocation();
   const { pathname } = location;
   const { data: books = [], isLoading: isBooksLoading } = useFetchBooksQuery();
   const { data: patrons = [], isLoading: isPatronsLoading } = useFetchPatronsQuery();
 
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer); // Cleanup timer on unmount
+  }, []);
+
+  const formatTime = (time) => {
+    return time.toLocaleTimeString();
+  };
+
+  const formatDate = (date) => {
+    return date.toLocaleDateString();
+  };
   return (
     <div className="container">
       <div className="row pt-3">
       <div className="col-xl-6 col-md-6 mb-4">
  {/* Books Section */}
- <div className="col-xl-4 col-md-12 mb-4">
+ <div className="row">
+
+ <div className="col-xl-12 col-md-12 mb-4">
+ {formatDate(currentTime)} {formatTime(currentTime)}
+  </div>
+
+ <div className="col-xl-6 col-md-12 mb-4">
           <NavLink to="/patron/allbooks" style={{ textDecoration: "none" }}>
             <div className="card h-100">
               <div className="card-body">
@@ -40,7 +66,7 @@ const IndexPatronScreen = () => {
         </div>
 
         {/* Overdue Section */}
-        <div className="col-xl-4 col-md-12 mb-4">
+        <div className="col-xl-6 col-md-12 mb-4">
           <NavLink to="/patron/overdue" style={{ textDecoration: "none" }}>
             <div className="card h-100">
               <div className="card-body">
@@ -60,7 +86,7 @@ const IndexPatronScreen = () => {
     
 
         {/* Transactions Section */}
-        <div className="col-xl-4 col-md-12 mb-4">
+        <div className="col-xl-12 col-md-12 mb-4">
 
               <NavLink to="/patron/transactions" style={{ textDecoration: "none" }}>
                 <div className="card h-100">
@@ -78,7 +104,7 @@ const IndexPatronScreen = () => {
               </NavLink>
 
         </div>
-
+        </div>
         </div>
         <div className="col-xl-6 col-md-6 mb-4">
         <Calendar  />
